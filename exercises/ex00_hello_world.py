31d1060dedce4da38bf1f24e7faddbd97b847c7e
lglazer <eglazer@ad.unc.edu>
2024-08-21 10:31:00 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730567393"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
