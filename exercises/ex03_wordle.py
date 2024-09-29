7591b44e85b52330ec4351609a87c41046df726b
lglazer <eglazer@ad.unc.edu>
2024-09-29 12:42:45 -0400

    Finalized EX03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -4,7 +4,7 @@ __author__ = "730567393"
 
 
 def input_guess(secret_word_len: int) -> str:
-    input_word: str = input(f"Enter a {secret_word_len}-character word: ")
+    input_word: str = input(f"Enter a {secret_word_len} character word: ")
     # creates a global variable called word that takes an input after
     # prompted with a message to enter a word of a specific length
     while len(input_word) != secret_word_len:
@@ -66,18 +66,20 @@ def emojified(guess: str, secret: str) -> str:
                 # if statement that adds a Green box emoji to the originally empty
                 # emoji string variable if the guess character at a particular index
                 # is the same as the secret character at that index
-                # (for example: if a is the second letter in both the guess word and secret word)
+                # (for example: if a is the second letter in both the guess word
+                # and secret word)
             else:
                 emoji_str += YELLOW_BOX
             num_char += 1
-            # the corresponding else statement adds a Yellow box emoji to the emoji string variable
-            # if the guess character is in the secret word but in a different place
-            # then it adds one to the index variable to iterate through each character of the word
+            # the corresponding else statement adds a Yellow box emoji to the emoji
+            # string variable if the guess character is in the secret word but in
+            # a different place then it adds one to the index variable to iterate
+            # through each character of the word
         else:
             emoji_str += WHITE_BOX
             num_char += 1
-            # an else statement is used if the guess character is not found in the secret word,
-            # meaning if the contains_char is == False
+            # an else statement is used if the guess character is not found in
+            # the secret word, meaning if the contains_char is == False
     print(emoji_str)
     return emoji_str
     # this is the return statement that prints the completed emoji string
@@ -89,22 +91,25 @@ def main(secret: str) -> None:
     # creates a variable N that starts at 1 and counts the number of turns
     while N <= 6:
         print(f"=== Turn {N}/6 ===")
-        # creates a while loop that first prints a statement saying which turn the user is on
+        # creates a while loop that first prints a statement
+        # saying which turn the user is on
         guess: str = input_guess(len(secret))
-        # defines a local variable that takes the value of the input of the function input_guess
+        # defines a local variable that takes the value of the input of the
+        # function input_guess
         emojified(guess, secret)
         # calls the emojified function with the inputted guess
-        # adds one to the variable counting the number of turns after
-        # completing the function calls which check for character matches
-        # in the inputted guessed word and the defined secret word
         if guess == secret:
             print(f"You won in {N}/6 turns!")
             return
-            # the if statement checks to see if the guessed word is the same as the secret word
-            # if that is true and the inputted guess matches the secret word, it prints a statement
-            # saying that the user won in a specific number of turns using the N variable that
-            # was used to count turns. then the program is exited
+            # the if statement checks to see if the guessed word is the same as the
+            # secret word if that is true and the inputted guess matches the secret
+            # word, it prints a statement saying that the user won in a specific number
+            # of turns using the N variable that was used to count turns.
+            # then the program is exited
         N += 1
+        # adds one to the variable counting the number of turns after
+        # completing the function calls which check for character matches
+        # in the inputted guessed word and the defined secret word
     print("X/6 - Sorry, try again tomorrow!")
     # in the case where the inputted guess is not the secret word after 6 tries,
     # the function prints a statement to share that their guesses were not correct
